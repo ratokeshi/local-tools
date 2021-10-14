095422eacfbac7edb938349a72cb05f8632efdfa
Roy Tokeshi <roy.tokeshi@gmail.com>
2021-10-14 10:51:07 -0700

    Update timestamp to seconds

--- cr.py
+++ cr.py
@@ -1,4 +1,11 @@
-import datetime
+import datetime 
+import pyperclip
 
 now = datetime.datetime.now()
-print (now.year,now.month,now.day,'-',now.hour,now.minute,sep='')
+currenttimestamp = str(now.year) + str(now.month) + str(now.day) + '-' + str(now.hour) + str(now.minute) + '-' + str(now.second)
+#print (now.year,now.month,now.day,'-',now.hour,now.minute,'-',now.second,sep='')
+print (currenttimestamp)
+
+
+pyperclip.copy(currenttimestamp)
+spam = pyperclip.paste()
