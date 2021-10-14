e387b9c35d370adca7aa3ea0e359108ea4e65aa3
Roy Tokeshi <roy.tokeshi@gmail.com>
2021-10-14 14:17:09 -0700

    update hour to 24 hour format and use strftime

--- cr.py
+++ cr.py
@@ -4,8 +4,10 @@ import pyperclip
 now = datetime.datetime.now()
 currenttimestamp = str(now.year) + str(now.month) + str(now.day) + '-' + str(now.hour) + str(now.minute) + '-' + str(now.second)
 #print (now.year,now.month,now.day,'-',now.hour,now.minute,'-',now.second,sep='')
-print (currenttimestamp)
+#print (currenttimestamp)
+localtimestamp = now.strftime("%Y%m%d-%H%M-%S")
 
+print(localtimestamp)
 
-pyperclip.copy(currenttimestamp)
+pyperclip.copy(localtimestamp)
 spam = pyperclip.paste()
